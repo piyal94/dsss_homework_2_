9afb644bb2b0c36430a8dda6ed3d0df3e0da923a
piyal94 <95497035+piyal94@users.noreply.github.com>
2024-11-10 18:05:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',
+        ],
+    },
+    install_requires=[
+        # Add your dependencies here if you have any
+    ],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple math quiz game',
+    license='MIT',
+    url='https://github.com/piyal94/dsss_homework_2_',
+)
